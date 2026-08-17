f8399dfa18bb2dc6d2a321674f834d96eb52dd06
DevbratMani01 <parthmani002@gmail.com>
2026-08-17 21:12:07 +0530

    .

---
+++ arithimatic.py
@@ -0,0 +1,65 @@
+a = 17
+b = 5
+
+print(f"a + b = {a + b}") # addition
+print(f"a - b = {a - b}") # subtraction
+print(f"a * b = {a * b}") # multiplication
+print(f"a / b = {a / b}") # division
+print(f"a // b = {a // b}") # floor division
+print(f"a % b = {a % b}") # modulus
+print(f"a ** b = {a ** b}") # exponentiation
+
+a = 56
+b = 12
+
+print(f"a > b = {a > b}") # greater than
+print(f"a < b = {a < b}") # less than
+print(f"a == b = {a == b}") # equal to
+print(f"a != b = {a != b}") # not equal to
+print(f"a >= b = {a >= b}") # greater than or equal to
+print(f"a <= b = {a <= b}") # less than or equal to
+
+
+# Membership and identity operator
+
+subjects = ["Maths" , "Physics" , "Python"]
+
+print("biology" in subjects)
+print("Maths" in subjects)
+
+# Q1
+
+price = 250
+quantity = 3
+
+print(price + quantity)
+
+
+#Q2
+
+num = 91
+if(num % 2 == 0):
+    print (True)
+else:
+    print (False)
+
+
+#Q3
+
+Marks = int (input("Enter your marks : "))
+attendence = int (input("Enter your attendence : "))
+
+print(Marks >= 40 and attendence >= 75)
+
+
+#Q4
+
+subjects = ["Maths" , "Physics" , "Python"]
+
+print("Chemistry" in subjects)
+
+
+
+
+
+
---
+++ variable.py
@@ -0,0 +1,55 @@
+age = 19
+print(age)
+
+print(type(age))
+
+age = 20
+
+print("Age is now:", age)
+
+marks = 87
+percentage = 87.5
+student = "John Doe"
+is_passed = True
+
+print(type(marks))
+print(type(percentage))
+print(type(student))
+print(type(is_passed))
+
+text_number = "123"
+real_number = int(text_number)
+
+print(text_number + text_number)
+print(real_number + real_number)
+
+
+name = "Arav"
+score = 87
+
+print(f"{name} scored {score} marks in the exam.")
+print(f"Next year, {name} aims to score {score + 5} marks.")
+
+user_name = input("Enter your name: ")
+print(f"Hello, {user_name}! Welcome to the program.")
+
+
+#Q1
+city = input("Enter your city: ")
+print(f"You live in {city}.")
+
+#Q2
+a = 21
+b = 40.54
+
+print(type(a))
+print(type(b))
+
+#Q3
+str = "3.14"
+d = float(str)
+print(d*2)
+
+#Q4
+age = 19
+print(f"My city is {city} and I am {age} years old.")
